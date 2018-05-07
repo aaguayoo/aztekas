dc7c2e5dd0d5e77693d41ee43d4377321e82f23d
aaguayoo <39069055+aaguayoo@users.noreply.github.com>
2018-05-07 16:48:42 -0500

    Add files via upload

---
+++ test/Headers/param.h
@@ -0,0 +1,28 @@
+//DOMAIN//
+#define x1max 10.0
+#define x2max 20.0
+#define x3max 1.0
+#define x1min 0.0
+#define x2min 0.0
+#define x3min 0.0
+
+//MESH//
+#define Nx1 166
+#define Nx2 406
+#define Nx3 0
+
+//MAX TIME & PRINT TIME//
+#define tmax      30
+#define timefile  0.1
+
+//DIMENSION PARAMETERS (num of equations, dim of graf, physical dimension)//
+#define eq    4
+#define graf  2
+#define dim   2
+#define alfa  1
+
+//COURANT NUMBER//
+#define cou   0.2
+
+//POLYTROPIC INDEX//
+#define K  1.66666666666666666666666666666666
