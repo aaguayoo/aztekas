f1a504b38c3db960f354d9ed0667ae7fc0a0d1d8
Alejandro Aguayo-Ortiz <aaguayo@astro.unam.mx>
2018-05-07 17:08:35 -0500

    Se agregó aztekas.

---
+++ AZTEKAS-2.0-ALL/Headers/vector.h
@@ -0,0 +1,5 @@
+double funct_Q(double *a, double *uu);
+double funct_F(double *a, double *uu);
+double funct_G(double *a, double *uu);
+double funct_H(double *a, double *uu);
+double funct_S(double *a, double *uu);
